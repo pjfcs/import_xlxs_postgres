c77c86a756dccbc4759d13b3ccb13b747ef6a571
root <root@S_8925.sema-ba.intranet>
2022-03-18 17:08:45 -0300

    Second commit

--- PostgreSQL/scripts/createDB.sh
+++ PostgreSQL/scripts/createDB.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER paradox_user WITH PASSWORD '1234';
-    CREATE DATABASE db_paradox OWNER paradox_user;
+    CREATE USER userGestor WITH PASSWORD '1234';
+    CREATE DATABASE db_Fiplan OWNER userGestor;
 EOSQL
