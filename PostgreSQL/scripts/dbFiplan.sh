da24a41e235d3c473e7b1657a0373e6b591f2860
Pedro Jose Freire Castro <pjfcastro@gmail.com>
2022-09-23 10:37:24 -0300

    New adjustments.

--- PostgreSQL/scripts/dbFiplan.sh
+++ PostgreSQL/scripts/dbFiplan.sh
@@ -308,206 +308,206 @@ CREATE TABLE fiplan.orgao(
     comment on table fiplan.fiplan61 
     is 'Tabela para receber os dados da planilha61 do fiplan, exportada pela APG.';
 
-    comment on column fiplan.fiplan61.id_fiplan61                          
-    is "Identificador unívoco do registro. Surrogate key.";
+        comment on column fiplan.fiplan61.id_fiplan61                          
+    is 'Identificador unívoco do registro. Surrogate key.';
     comment on column fiplan.fiplan61.exercicio                            
-    is "Ano de referência.";
+    is 'Ano de referência.';
     comment on column fiplan.fiplan61.codigo_do_poder                      
-    is "Código do Poder.";
+    is 'Código do Poder.';
     comment on column fiplan.fiplan61.nome_do_poder                        
-    is "Nome do Poder.";
+    is 'Nome do Poder.';
     comment on column fiplan.fiplan61.codigo_do_orgao                      
-    is "Código do Órgão.";
+    is 'Código do Órgão.';
     comment on column fiplan.fiplan61.nome_do_orgao                        
-    is "Nome do Órgão.";
+    is 'Nome do Órgão.';
     comment on column fiplan.fiplan61.sigla_do_orgao                       
-    is "Sigla do Órgão.";
+    is 'Sigla do Órgão.';
     comment on column fiplan.fiplan61.codigo_da_uo                         
-    is "Código da Unidade Orçamentária.";
+    is 'Código da Unidade Orçamentária.';
     comment on column fiplan.fiplan61.nome_da_uo                           
-    is "Nome da Unidade Orçamentária.";
+    is 'Nome da Unidade Orçamentária.';
     comment on column fiplan.fiplan61.sigla_da_uo                          
-    is "Sigla da Unidade Orçamentária.";
+    is 'Sigla da Unidade Orçamentária.';
     comment on column fiplan.fiplan61.codigo_da_ug                         
-    is "Código da Unidade Gestora.";
+    is 'Código da Unidade Gestora.';
     comment on column fiplan.fiplan61.nome_da_ug                           
-    is "Nome da Unidade Gestora.";
+    is 'Nome da Unidade Gestora.';
     comment on column fiplan.fiplan61.sigla_da_ug                          
-    is "Sigla da Unidade Gestora.";
+    is 'Sigla da Unidade Gestora.';
     comment on column fiplan.fiplan61.codigo_do_orgao_de_destino           
-    is "Código do Órgão de Destino.";
+    is 'Código do Órgão de Destino.';
     comment on column fiplan.fiplan61.nome_do_orgao_de_destino             
-    is "Nome do Órgão de Destino.";
+    is 'Nome do Órgão de Destino.';
     comment on column fiplan.fiplan61.sigla_do_orgao_de_destino            
-    is "Sigla do Órgão de Destino.";
+    is 'Sigla do Órgão de Destino.';
     comment on column fiplan.fiplan61.codigo_da_uo_destino                 
-    is "Código da Unidade Orçamentária de destino.";
+    is 'Código da Unidade Orçamentária de destino.';
     comment on column fiplan.fiplan61.nome_da_uo_destino                   
-    is "Nome da Unidade Orçamentária de destino.";
+    is 'Nome da Unidade Orçamentária de destino.';
     comment on column fiplan.fiplan61.sigla_da_uo_destino                  
-    is "Sigla da Unidade Orçamentária de destino.";
+    is 'Sigla da Unidade Orçamentária de destino.';
     comment on column fiplan.fiplan61.codigo_da_ug_destino                 
-    is "Código da Unidade Gestora de destino.";
+    is 'Código da Unidade Gestora de destino.';
     comment on column fiplan.fiplan61.nome_da_ug_destino                   
-    is "Nome da Unidade Gestora de destino.";
+    is 'Nome da Unidade Gestora de destino.';
     comment on column fiplan.fiplan61.sigla_da_ug_destino                  
-    is "Sigla da Unidade Gestora de destino.";
+    is 'Sigla da Unidade Gestora de destino.';
     comment on column fiplan.fiplan61.codigo_da_usp                        
-    is "Código da Unidade Setorial de Planejamento.";
+    is 'Código da Unidade Setorial de Planejamento.';
     comment on column fiplan.fiplan61.nome_da_usp                          
-    is "Nome da Unidade Setorial de Planejamento.";
+    is 'Nome da Unidade Setorial de Planejamento.';
     comment on column fiplan.fiplan61.sigla_da_usp                         
-    is "Sigla da Unidade Setorial de Planejamento.";
+    is 'Sigla da Unidade Setorial de Planejamento.';
     comment on column fiplan.fiplan61.tipo_da_acao                         
-    is "Tipo da Ação Orçamentária.";
+    is 'Tipo da Ação Orçamentária.';
     comment on column fiplan.fiplan61.codigo_da_acao                       
-    is "Código da Ação Orçamentária.";
+    is 'Código da Ação Orçamentária.';
     comment on column fiplan.fiplan61.nome_da_acao                         
-    is "Nome da Ação Orçamentária.";
+    is 'Nome da Ação Orçamentária.';
     comment on column fiplan.fiplan61.sigla_da_acao                        
-    is "Nome resumido da Ação Orçamentária.";
+    is 'Nome resumido da Ação Orçamentária.';
     comment on column fiplan.fiplan61.objetivo_da_acao                     
-    is "Objetivo da Ação Orçamentária.";
+    is 'Objetivo da Ação Orçamentária.';
     comment on column fiplan.fiplan61.classificacao_da_acao                
-    is "Classificação da Ação Orçamentária.";
+    is 'Classificação da Ação Orçamentária.';
     comment on column fiplan.fiplan61.detalhamento_da_acao                 
-    is "Detalhamento da Ação Orçamentária.";
+    is 'Detalhamento da Ação Orçamentária.';
     comment on column fiplan.fiplan61.indicativo_de_acao_prioritaria       
-    is "Indicativo de Ação Prioritária.";
+    is 'Indicativo de Ação Prioritária.';
     comment on column fiplan.fiplan61.codigo_da_prioridade_de_governo      
-    is "Código da Prioridade de governo.";
+    is 'Código da Prioridade de governo.';
     comment on column fiplan.fiplan61.prioridade                           
-    is "Prioridade de governo.";
+    is 'Prioridade de governo.';
     comment on column fiplan.fiplan61.esfera_orcamentaria                  
-    is "Esfera Orçamentária.";
+    is 'Esfera Orçamentária.';
     comment on column fiplan.fiplan61.codigo_da_funcao                     
-    is "Código da Função de governo.";
+    is 'Código da Função de governo.';
     comment on column fiplan.fiplan61.nome_da_funcao                       
-    is "Nome da Função de governo.";
+    is 'Nome da Função de governo.';
     comment on column fiplan.fiplan61.sigla_da_funcao                      
-    is "Nome Resumido da Função de governo.";
+    is 'Nome Resumido da Função de governo.';
     comment on column fiplan.fiplan61.codigo_da_subfuncao                  
-    is "Código da Subfunção.";
+    is 'Código da Subfunção.';
     comment on column fiplan.fiplan61.nome_da_subfuncao                    
-    is "Nome da Subfunção.";
+    is 'Nome da Subfunção.';
     comment on column fiplan.fiplan61.sigla_da_subfuncao                   
-    is "Nome Resumido da Subfunção.";
+    is 'Nome Resumido da Subfunção.';
     comment on column fiplan.fiplan61.codigo_do_programa                   
-    is "Código do Programa.";
+    is 'Código do Programa.';
     comment on column fiplan.fiplan61.nome_do_programa                     
-    is "Nome do Programa.";
+    is 'Nome do Programa.';
     comment on column fiplan.fiplan61.sigla_do_programa                    
-    is "Nome Resumido do Programa.";
+    is 'Nome Resumido do Programa.';
     comment on column fiplan.fiplan61.codigo_do_produto                    
-    is "Código do Produto.";
+    is 'Código do Produto.';
     comment on column fiplan.fiplan61.nome_do_produto                      
-    is "Nome do Produto.";
+    is 'Nome do Produto.';
     comment on column fiplan.fiplan61.sigla_do_produto                     
-    is "Nome Resumido do Produto.";
+    is 'Nome Resumido do Produto.';
     comment on column fiplan.fiplan61.codigo_da_unidade_de_medida          
-    is "Código da Unidade de Medida.";
+    is 'Código da Unidade de Medida.';
     comment on column fiplan.fiplan61.nome_da_unidade_de_medida            
-    is "Nome da Unidade de Medida.";
+    is 'Nome da Unidade de Medida.';
     comment on column fiplan.fiplan61.sigla_da_unidade_de_medida           
-    is "Sigla da Unidade de Medida.";
+    is 'Sigla da Unidade de Medida.';
     comment on column fiplan.fiplan61.codigo_da_regiao                     
-    is "Código da Região.";
+    is 'Código da Região.';
     comment on column fiplan.fiplan61.nome_da_regiao                       
-    is "Nome da Região.";
+    is 'Nome da Região.';
     comment on column fiplan.fiplan61.codigo_da_destinacao                 
-    is "Código da Destinação.";
+    is 'Código da Destinação.';
     comment on column fiplan.fiplan61.codigo_do_identificador_de_exercicio 
-    is "Código do Identificador de Exercício.";
+    is 'Código do Identificador de Exercício.';
     comment on column fiplan.fiplan61.codigo_da_ftfe                       
-    is "Código da Fonte de Recurso Federação.";
+    is 'Código da Fonte de Recurso Federação.';
     comment on column fiplan.fiplan61.nome_da_ftfe                         
-    is "Nome da Fonte de Recurso Federação.";
+    is 'Nome da Fonte de Recurso Federação.';
     comment on column fiplan.fiplan61.nome_resumido_da_ftfe                
-    is "Nome resumido da Fonte de Recurso Federação.";
+    is 'Nome resumido da Fonte de Recurso Federação.';
     comment on column fiplan.fiplan61.codigo_do_identificador_de_uso       
-    is "Código do Identificador de Uso.";
+    is 'Código do Identificador de Uso.';
     comment on column fiplan.fiplan61.codigo_da_fonte                      
-    is "Código da Fonte de Recurso Estadual.";
+    is 'Código da Fonte de Recurso Estadual.';
     comment on column fiplan.fiplan61.nome_da_fonte                        
-    is "Nome da Fonte de Recurso Estadual.";
+    is 'Nome da Fonte de Recurso Estadual.';
     comment on column fiplan.fiplan61.sigla_da_fonte                       
-    is "Nome resumido da Fonte de Recurso Estadual.";
+    is 'Nome resumido da Fonte de Recurso Estadual.';
     comment on column fiplan.fiplan61.codigo_do_tiac                       
-    is "Código da Subfonte de recurso.";
+    is 'Código da Subfonte de recurso.';
     comment on column fiplan.fiplan61.nome_do_tiac                         
-    is "Nome da Subfonte de recurso.";
+    is 'Nome da Subfonte de recurso.';
     comment on column fiplan.fiplan61.codigo_do_diac                       
-    is "Nome resumido da Subfonte de recurso.";
+    is 'Nome resumido da Subfonte de recurso.';
     comment on column fiplan.fiplan61.nome_do_diac                         
-    is "Código do Tipo de Identificador de Acompanhamento.";
+    is 'Código do Tipo de Identificador de Acompanhamento.';
     comment on column fiplan.fiplan61.codigo_do_ia                         
-    is "Nome do Tipo de Identificador de Acompanhamento.";
+    is 'Nome do Tipo de Identificador de Acompanhamento.';
     comment on column fiplan.fiplan61.nome_do_ia                           
-    is "Código do Detalhamento do Identificador de Acompanhamento.";
+    is 'Código do Detalhamento do Identificador de Acompanhamento.';
     comment on column fiplan.fiplan61.codigo_da_subfonte                   
-    is "Nome do Detalhamento do Identificador de Acompanhamento.";
+    is 'Nome do Detalhamento do Identificador de Acompanhamento.';
     comment on column fiplan.fiplan61.nome_da_subfonte                     
-    is "Código da Informação Adicional.";
+    is 'Código da Informação Adicional.';
     comment on column fiplan.fiplan61.sigla_da_subfonte                    
-    is "Nome da Informação Adicional.";
+    is 'Nome da Informação Adicional.';
     comment on column fiplan.fiplan61.codigo_da_natureza                   
-    is "Código da Natureza da despesa.";
+    is 'Código da Natureza da despesa.';
     comment on column fiplan.fiplan61.nome__da_natureza                    
-    is "Nome da Natureza da despesa.";
+    is 'Nome da Natureza da despesa.';
     comment on column fiplan.fiplan61.sigla_da_natureza                    
-    is "Nome resumido da Natureza da despesa.";
+    is 'Nome resumido da Natureza da despesa.';
     comment on column fiplan.fiplan61.codigo_da_categoria                  
-    is "Código da Categoria Econômica.";
+    is 'Código da Categoria Econômica.';
     comment on column fiplan.fiplan61.nome__da_categoria_economica         
-    is "Nome  da Categoria Econômica.";
+    is 'Nome  da Categoria Econômica.';
     comment on column fiplan.fiplan61.codigo_do_grupo                      
-    is "Código do Grupo de despesa.";
+    is 'Código do Grupo de despesa.';
     comment on column fiplan.fiplan61.nome_do_grupo                        
-    is "Nome do Grupo de despesa.";
+    is 'Nome do Grupo de despesa.';
     comment on column fiplan.fiplan61.sigla_do_grupo                       
-    is "Nome resumido do Grupo de despesa.";
+    is 'Nome resumido do Grupo de despesa.';
     comment on column fiplan.fiplan61.codigo_da_modalidade                 
-    is "Código da Modalidade de aplicação.";
+    is 'Código da Modalidade de aplicação.';
     comment on column fiplan.fiplan61.nome_da_modalidade                   
-    is "Nome da Modalidade de aplicação.";
+    is 'Nome da Modalidade de aplicação.';
     comment on column fiplan.fiplan61.sigla_da_modalidade                  
-    is "Nome resumido da Modalidade de aplicação.";
+    is 'Nome resumido da Modalidade de aplicação.';
     comment on column fiplan.fiplan61.codigo_do_elemento                   
-    is "Código do Elemento de despesa.";
+    is 'Código do Elemento de despesa.';
     comment on column fiplan.fiplan61.nome_do_elemento                     
-    is "Nome do Elemento de despesa.";
+    is 'Nome do Elemento de despesa.';
     comment on column fiplan.fiplan61.sigla_do_elemento                    
-    is "Nome resumido do Elemento de despesa.";
+    is 'Nome resumido do Elemento de despesa.';
     comment on column fiplan.fiplan61.codigo_do_tipo_de_gasto              
-    is "Código do Tipo de Gasto.";
+    is 'Código do Tipo de Gasto.';
     comment on column fiplan.fiplan61.descricao_do_tipo_de_gasto           
-    is "Descrição do Tipo de Gasto.";
+    is 'Descrição do Tipo de Gasto.';
     comment on column fiplan.fiplan61.valor_orcado_inicial                 
-    is "Valor Orçado Inicial ( LOA ).";
+    is 'Valor Orçado Inicial ( LOA ).';
     comment on column fiplan.fiplan61.valor_suplementado                   
-    is "Valor Suplementado.";
+    is 'Valor Suplementado.';
     comment on column fiplan.fiplan61.valor_anulado                        
-    is "Valor Anulado.";
+    is 'Valor Anulado.';
     comment on column fiplan.fiplan61.valor_atual                          
-    is "Valor Atual .";
+    is 'Valor Atual .';
     comment on column fiplan.fiplan61.valor_descentralizado                
-    is "Valor Descentralizado.";
+    is 'Valor Descentralizado.';
     comment on column fiplan.fiplan61.valor_bloqueado                      
-    is "Valor Bloqueado.";
+    is 'Valor Bloqueado.';
     comment on column fiplan.fiplan61.valor_contingenciado                 
-    is "Valor Contingenciado.";
+    is 'Valor Contingenciado.';
     comment on column fiplan.fiplan61.valor_ped                            
-    is "Valor PED - Pedido de Empenho.";
+    is 'Valor PED - Pedido de Empenho.';
     comment on column fiplan.fiplan61.valor_empenhado                      
-    is "Valor Empenhado.";
+    is 'Valor Empenhado.';
     comment on column fiplan.fiplan61.valor_liquidado                      
-    is "Valor Liquidado.";
+    is 'Valor Liquidado.';
     comment on column fiplan.fiplan61.valor_pago                           
-    is "Valor Pago.";
+    is 'Valor Pago.';
     comment on column fiplan.fiplan61.data_de_extracao_de_dados            
-    is "Data de Extração de Dados.";
+    is 'Data de Extração de Dados.';
     comment on column fiplan.fiplan61.hora_de_extracao_de_dados            
-    is "Hora de Extração de Dados.";
+    is 'Hora de Extração de Dados.';
     comment on column fiplan.fiplan61.dt_cadastro                          
-    is "Data e hora de inserção do registro na tabela fiplan.fiplan61.";
+    is 'Data e hora de inserção do registro na tabela fiplan.fiplan61.';
 EOSQL
